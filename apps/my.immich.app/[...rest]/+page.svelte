561745f436503df11ac74963db094193833a3916
Jason Rasmussen <jason@rasm.me>
2025-01-03 12:28:01 -0500

    feat: supporter badge title

--- apps/my.immich.app/[...rest]/+page.svelte
+++ apps/my.immich.app/[...rest]/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
   import { StorageKey } from '$lib';
   import '$lib/app.css';
-  import { Button, Card, CardBody, Field, Heading, HelperText, Input, Logo, Stack, Text } from '@immich/ui';
+  import {
+    Button,
+    Card,
+    CardBody,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+    Heading,
+    Icon,
+    Input,
+    Logo,
+    Stack,
+    SupporterBadge,
+    Text,
+  } from '@immich/ui';
+  import { mdiCheckCircleOutline } from '@mdi/js';
   import type { PageData } from './$types';
 
   interface Props {
@@ -25,46 +41,50 @@
     } else {
       saved = true;
     }
+
+    setTimeout(() => {
+      saved = false;
+    }, 3000);
   };
 </script>
 
-<form onsubmit={handleSubmit}>
-  <div class="p-4 md:p-12 lg:p-24">
-    <div class="mx-auto max-w-screen-sm">
+<div class="h-full mx-auto max-w-screen-sm p-4">
+  <Stack gap={8}>
+    <SupporterBadge effect="always">
+      <Logo size="large" variant="icon" />
+      <Heading size="large" color="primary">My Immich</Heading>
+    </SupporterBadge>
+    <Text>My Immich allows public links to link you to specific areas of your personal Immich instance.</Text>
+
+    <form onsubmit={handleSubmit}>
       <Card color="secondary" variant="subtle">
-        <CardBody class="p-4 lg:p-8">
+        <CardHeader>
+          <CardTitle>Instance URL</CardTitle>
+          <CardDescription>This URL is only stored in your browser</CardDescription>
+        </CardHeader>
+        <CardBody>
           <section>
-            <div class="flex justify-center">
-              <Logo variant="stacked" theme="dark" size="landing" />
-            </div>
             <Stack gap={4}>
-              <Heading size="large" color="primary">My Immich</Heading>
-
-              <Text>My Immich allows public links to link you to specific areas of your personal Immich instance.</Text>
-
-              <Field label="Instance URL">
-                <Input
-                  type="text"
-                  placeholder="https://demo.immich.app/"
-                  bind:value={instanceUrl}
-                  oninput={handleChange}
-                />
-                <HelperText>Note: This URL is only stored in your browser.</HelperText>
-              </Field>
-
-              <div class="flex justify-end">
-                {#if saved}
-                  <Text size="small" color="primary">Saved!</Text>
-                {:else}
-                  <Button type="submit" class="w-full sm:w-auto">
-                    {targetUrl ? 'Save & Redirect' : 'Save'}
-                  </Button>
-                {/if}
-              </div>
+              <Input
+                type="text"
+                placeholder="https://demo.immich.app/"
+                bind:value={instanceUrl}
+                oninput={handleChange}
+              />
             </Stack>
           </section>
         </CardBody>
+        <CardFooter class="justify-end">
+          <Button type="submit" class="w-full sm:w-auto" disabled={saved}>
+            {#if saved}
+              <Icon icon={mdiCheckCircleOutline} />
+              Saved!
+            {:else}
+              {targetUrl ? 'Save & Redirect' : 'Save'}
+            {/if}
+          </Button>
+        </CardFooter>
       </Card>
-    </div>
-  </div>
-</form>
+    </form>
+  </Stack>
+</div>
