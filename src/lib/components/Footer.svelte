3b9945c757f41367319055cfb20ba2db386aac88
Jason Rasmussen <jason@rasm.me>
2025-01-03 16:48:11 -0500

    feat: page improvements (#83)

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,124 @@
+<script lang="ts">
+  import { Card, Heading, HStack, Icon, IconButton, Link, Stack, Text, theme, Theme, VStack } from '@immich/ui';
+  import {
+    mdiAndroid,
+    mdiApple,
+    mdiGithub,
+    mdiMessageOutline,
+    mdiOfficeBuildingOutline,
+    mdiReddit,
+    mdiServerOutline,
+    mdiWeatherNight,
+    mdiWeatherSunny,
+    mdiWeb,
+  } from '@mdi/js';
+
+  const handleToggleTheme = () => {
+    theme.value = theme.value === Theme.Dark ? Theme.Light : Theme.Dark;
+  };
+
+  const themeIcon = $derived(theme.value === Theme.Light ? mdiWeatherSunny : mdiWeatherNight);
+</script>
+
+<div class="bg-dark/10 rounded-t-3xl mt-16 p-8">
+  <div class="max-w-screen-md mx-auto lg:py-8">
+    <Stack gap={8}>
+      <div class="grid grid-cols-2 gap-8 place-center lg:grid-cols-4">
+        <Stack>
+          <Heading size="tiny">Social</Heading>
+          <Link href="https://github.com/immich-app/immich" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={mdiGithub} size="1.5em" />
+              <Text>GitHub</Text>
+            </HStack>
+          </Link>
+          <Link href="https://discord.immich.app/" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={mdiMessageOutline} size="1.5em" />
+              <Text>Discord</Text>
+            </HStack>
+          </Link>
+          <Link href="https://www.reddit.com/r/immich/" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={mdiReddit} size="1.5em" />
+              <Text>Reddit</Text>
+            </HStack>
+          </Link>
+        </Stack>
+
+        <Stack>
+          <Heading size="tiny">Download</Heading>
+          <Link
+            href="https://play.google.com/store/apps/details?id=app.alextran.immich"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <HStack>
+              <Icon icon={mdiAndroid} size="1.5em" />
+              <Text>Android</Text>
+            </HStack>
+          </Link>
+          <Link href="https://apps.apple.com/us/app/immich/id1613945652" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={mdiApple} size="1.5em" />
+              <Text>iOS</Text>
+            </HStack>
+          </Link>
+          <Link href="https://get.immich.app/docker-compose" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={mdiServerOutline} size="1.5em" />
+              <Text>Server</Text>
+            </HStack>
+          </Link>
+        </Stack>
+
+        <Stack>
+          <Heading size="tiny">Sites</Heading>
+          <Link href="https://immich.app" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={mdiWeb} size="1.5em" />
+              <Text>Documentation</Text>
+            </HStack>
+          </Link>
+          <Link href="https://buy.immich.app" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={mdiWeb} size="1.5em" />
+              <Text>Buy Immich</Text>
+            </HStack>
+          </Link>
+          <Link href="https://my.immich.app" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={mdiWeb} size="1.5em" />
+              <Text>My Immich</Text>
+            </HStack>
+          </Link>
+        </Stack>
+
+        <Stack>
+          <Heading size="tiny">Company</Heading>
+          <Link href="https://futo.org/" target="_blank" rel="noopener noreferrer">
+            <HStack>
+              <Icon icon={mdiOfficeBuildingOutline} size="1.5em" />
+              <Text>FUTO</Text>
+            </HStack>
+          </Link>
+        </Stack>
+      </div>
+      <Card>
+        <img src="/img/social-preview.webp" alt="Social Preview" />
+      </Card>
+      <VStack class="text-center">
+        <Text size="large">This project is available under GNU AGPL v3 license.</Text>
+        <Text color="muted" variant="italic">Privacy should not be a luxury</Text>
+        <IconButton
+          size="large"
+          shape="round"
+          color="secondary"
+          variant="ghost"
+          icon={themeIcon}
+          onclick={handleToggleTheme}
+        />
+      </VStack>
+    </Stack>
+  </div>
+</div>
---
+++ src/lib/components/FullPageLayout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Footer from '$lib/components/Footer.svelte';
+  import { Scrollable, syncToDom } from '@immich/ui';
+  import type { Snippet } from 'svelte';
+
+  type Props = {
+    children: Snippet;
+  };
+
+  const { children }: Props = $props();
+
+  $effect(() => {
+    syncToDom();
+  });
+</script>
+
+<Scrollable>
+  <section class="flex flex-col h-dvh">
+    <div class="grow">
+      <div class="w-full h-full md:max-w-screen-md p-4 sm:p-8 lg:p-10 m-auto">
+        {@render children?.()}
+      </div>
+    </div>
+    <Footer />
+  </section>
+</Scrollable>
