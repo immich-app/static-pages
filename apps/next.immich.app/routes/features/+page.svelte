541f5bfb15cae0371a06d054e97fbf02f6810827
Jason Rasmussen <jason@rasm.me>
2025-01-21 16:59:24 -0500

    feat: more features (#97)

--- apps/next.immich.app/routes/features/+page.svelte
+++ apps/next.immich.app/routes/features/+page.svelte
@@ -15,22 +15,41 @@
     Text,
   } from '@immich/ui';
   import {
-    mdiAccountCircle,
     mdiAccountCog,
     mdiAccountMultiple,
+    mdiAccountMultipleOutline,
+    mdiAccountPlusOutline,
+    mdiArchiveOutline,
+    mdiCalendarTodayOutline,
     mdiCellphone,
     mdiChartLine,
-    mdiCloud,
     mdiCloudUpload,
+    mdiDatabaseOutline,
+    mdiEmailOutline,
     mdiFaceRecognition,
     mdiFileOutline,
+    mdiFileUploadOutline,
     mdiFolderMultiple,
+    mdiForumOutline,
+    mdiHeartOutline,
+    mdiImageAlbum,
     mdiImageMultiple,
+    mdiImageMultipleOutline,
+    mdiKeyOutline,
     mdiMagnify,
-    mdiMarker,
+    mdiMapMarkerOutline,
+    mdiMapOutline,
     mdiMotionPlayOutline,
+    mdiPanoramaOutline,
+    mdiPlayBoxOutline,
+    mdiRaw,
+    mdiRotate360,
+    mdiSecurity,
     mdiShareVariant,
     mdiTagMultiple,
+    mdiThemeLightDark,
+    mdiTimelineOutline,
+    mdiWeb,
   } from '@mdi/js';
 
   type Feature = {
@@ -41,14 +60,39 @@
 
   const features: Feature[] = [
     {
-      title: 'Automatic Backup',
-      description: 'Automatically backup your photos from your phone',
-      icon: mdiCloud,
+      title: 'Mobile App',
+      description: 'View your photos and videos on the go',
+      icon: mdiCellphone,
     },
     {
-      title: 'CLI upload',
-      description: 'Import photos and videos into Immich using the CLI',
-      icon: mdiCloudUpload,
+      title: 'Timeline',
+      description: 'View your photos and videos in a timeline',
+      icon: mdiTimelineOutline,
+    },
+    {
+      title: 'Albums',
+      description: 'Organize your photos and videos into albums and share them with other users',
+      icon: mdiImageAlbum,
+    },
+    {
+      title: 'Multi-User',
+      description: 'Add friends and family to your instance, each with their own private set of photos and videos',
+      icon: mdiAccountPlusOutline,
+    },
+    {
+      title: 'Shared Links',
+      description: 'Share your photos and videos with private, secure, and optionally password protected links',
+      icon: mdiShareVariant,
+    },
+    {
+      title: 'Partner Sharing',
+      description: 'Share your photos and videos with one or more partners',
+      icon: mdiAccountMultipleOutline,
+    },
+    {
+      title: 'Dark Theme',
+      description: 'View Immich using a dark theme',
+      icon: mdiThemeLightDark,
     },
     {
       title: 'Facial Recognition',
@@ -56,9 +100,120 @@
       icon: mdiFaceRecognition,
     },
     {
+      title: 'Map',
+      description: 'View your photos and videos on a map',
+      icon: mdiMapOutline,
+    },
+    {
+      title: 'Archive',
+      description: 'Hide non-essential photos and videos from the main timeline',
+      icon: mdiArchiveOutline,
+    },
+    {
+      title: 'Memories',
+      description: 'View photos and videos taken on this day in past years',
+      icon: mdiCalendarTodayOutline,
+    },
+    {
+      title: 'Favorites',
+      description: 'Keep track of your favorite photos and videos',
+      icon: mdiHeartOutline,
+    },
+    {
+      title: 'Smart Search',
+      description: 'Search your photos and videos with natural language',
+      icon: mdiMagnify,
+    },
+    {
+      title: 'Slideshow',
+      description: 'View your photos and videos as a slideshow',
+      icon: mdiPlayBoxOutline,
+    },
+    {
+      title: 'Live Photos',
+      description: 'View Live Photos from Apple devices',
+      icon: mdiMotionPlayOutline,
+    },
+    {
+      title: 'Motion Photos',
+      description: 'View Motion PHotos from Android devices',
+      icon: mdiMotionPlayOutline,
+    },
+    {
+      title: '360° Photos',
+      description: 'View your 360° photos in Immich',
+      icon: mdiRotate360,
+    },
+    {
+      title: 'Panoramas',
+      description: 'View your panoramas in Immich',
+      icon: mdiPanoramaOutline,
+    },
+    {
+      title: 'RAW',
+      description: 'View your raw, uncompressed photo and scans in Immich',
+      icon: mdiRaw,
+    },
+    {
+      title: 'Stacks',
+      description: 'Combine similar or related photos and videos into a stack',
+      icon: mdiImageMultipleOutline,
+    },
+    {
+      title: 'Tags',
+      description: 'Organize your photos and videos with hierarchal tags',
+      icon: mdiTagMultiple,
+    },
+    {
+      title: 'Drag and Drop',
+      description: 'Drag and drop photos, videos, and screenshots into Immich',
+      icon: mdiFileUploadOutline,
+    },
+    {
+      title: 'Internationalization',
+      description: 'View Immich in your preferred language and locale',
+      icon: mdiWeb,
+    },
+    {
+      title: 'Email Notifications',
+      description: 'Receive email notifications when new photos or videos are added to an album',
+      icon: mdiEmailOutline,
+    },
+    {
+      title: 'Trash',
+      description: 'Automatically delete photos and videos from the trash after a period of time',
+      icon: mdiTagMultiple,
+    },
+    {
+      title: 'Reverse Geocoding',
+      description: 'View and search your photos and videos by city, state, and country names',
+      icon: mdiMapMarkerOutline,
+    },
+    {
+      title: 'Activity & Comments',
+      description: 'View activity and comments on your photos and videos',
+      icon: mdiForumOutline,
+    },
+    {
+      title: 'External Libraries',
+      description: 'Mount your photos and videos as read-only, external folder',
+      icon: mdiImageMultiple,
+    },
+    {
       title: 'Folder View',
       description: 'View your photos and videos in a folder structure',
       icon: mdiFolderMultiple,
+    },
+    {
+      title: 'Storage Template',
+      description: 'Customize how your photos and videos are stored on disk',
+      icon: mdiTagMultiple,
+    },
+
+    {
+      title: 'OIDC',
+      description: 'Login to Immich using OpenID Connect',
+      icon: mdiSecurity,
     },
     {
       title: 'Hardware Transcoding',
@@ -66,49 +221,24 @@
       icon: mdiAccountMultiple,
     },
     {
-      title: 'External Libraries',
-      description: 'Mount external libraries as read-only',
-      icon: mdiImageMultiple,
+      title: 'Database Backups',
+      description: 'Automatically backup your Immich database',
+      icon: mdiDatabaseOutline,
     },
     {
-      title: 'Mobile App',
-      description: 'View your photos and videos on the go',
-      icon: mdiCellphone,
+      title: 'CLI Upload',
+      description: 'Import photos and videos into Immich using the CLI',
+      icon: mdiCloudUpload,
+    },
+    {
+      title: 'Api Keys',
+      description: 'Programmatically access your data or integrate Immich with third party services',
+      icon: mdiKeyOutline,
     },
     {
       title: 'Monitoring',
       description: 'Monitor the health of your Immich instance via Prometheus',
       icon: mdiChartLine,
-    },
-    {
-      title: 'Partner Sharing',
-      description: 'Share your photos and videos with a partner',
-      icon: mdiAccountCircle,
-    },
-    {
-      title: 'Reverse Geocoding',
-      description: 'Convert lat/long to city, state, and country names',
-      icon: mdiMarker,
-    },
-    {
-      title: 'Searching',
-      description: 'Search your photos and videos with natural language',
-      icon: mdiMagnify,
-    },
-    {
-      title: 'Sharing',
-      description: 'Share you photos and videos with other users, or public links',
-      icon: mdiShareVariant,
-    },
-    {
-      title: 'Motion Photos',
-      description: 'View Apple Live Photos and Google Motion Photos in Immich',
-      icon: mdiMotionPlayOutline,
-    },
-    {
-      title: 'Tags',
-      description: 'Organize your photos and videos with tags',
-      icon: mdiTagMultiple,
     },
     {
       title: 'User Settings',
