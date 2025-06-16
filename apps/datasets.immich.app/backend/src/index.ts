b9851b0a6b12eed743d4d66ba8ed2a94bca77173
bwees <brandonwees@gmail.com>
2025-06-16 14:05:05 -0500

    feat: add JWT auth and uploading

--- apps/datasets.immich.app/backend/src/index.ts
+++ apps/datasets.immich.app/backend/src/index.ts
@@ -1,7 +1,11 @@
-import { AutoRouter, IRequest } from 'itty-router';
+import { AutoRouter, cors, IRequest } from 'itty-router';
 import { exifRouter } from './datasets/exif';
+import { authRouter } from './auth';
 
-const router = AutoRouter<IRequest, [Env, ExecutionContext]>();
-router.all('/exif/*', exifRouter.fetch);
+const { preflight, corsify } = cors();
+
+const router = AutoRouter<IRequest, [Env, ExecutionContext]>({ before: [preflight], finally: [corsify] });
+router.all('/api/exif/*', exifRouter.fetch);
+router.all('/api/auth/*', authRouter.fetch);
 
 export default router;
