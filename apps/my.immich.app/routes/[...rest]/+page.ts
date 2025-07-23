5554f2224348af14c5882e0d3989570fd46c1ad3
bwees <brandonwees@gmail.com>
2025-07-23 10:17:28 -0500

    feat: support host/scheme params in my.immich.app URL

--- apps/my.immich.app/routes/[...rest]/+page.ts
+++ apps/my.immich.app/routes/[...rest]/+page.ts
@@ -5,6 +5,18 @@ import { StorageKey } from '$lib';
 export const ssr = false;
 
 export const load = (async ({ url }) => {
+  if (url.searchParams.has('host') && url.searchParams.has('scheme')) {
+    const host = url.searchParams.get('host');
+    const scheme = url.searchParams.get('scheme');
+
+    // we need search params without host and scheme
+    url.searchParams.delete('host');
+    url.searchParams.delete('scheme');
+    const strippedParams = url.searchParams.toString();
+
+    redirect(302, `${scheme}://${host}${url.pathname}?${strippedParams}`);
+  }
+
   const instanceUrl = localStorage.getItem(StorageKey.INSTANCE_URL) || '';
   const pathAndParams = url.pathname + url.search;
   const targetUrl = pathAndParams === '/' ? '' : pathAndParams;
