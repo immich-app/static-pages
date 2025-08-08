150c86d169f053e4fa5de04c8e5a8b1b310cf6ec
Jason Rasmussen <jason@rasm.me>
2025-08-08 08:27:13 -0400

    chore: remove claim page (#169)

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 process.env.PUBLIC_IMMICH_PAY_HOST = process.env.PUBLIC_IMMICH_PAY_HOST || 'https://pay.futo.org';
-process.env.PUBLIC_IMMICH_API_HOST = process.env.PUBLIC_IMMICH_API_HOST || 'https://api.immich.app';
 process.env.PUBLIC_IMMICH_SPEC_URL = process.env.PUBLIC_IMMICH_SPEC_URL || 'https://static.immich.cloud/docs/spec.json';
 
 const app = process.env.IMMICH_APP || 'my.immich.app';
