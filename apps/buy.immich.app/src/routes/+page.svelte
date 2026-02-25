016b28be1d16f39284334656e00fcaf8c8db6b70
Jason Rasmussen <jason@rasm.me>
2026-02-25 11:15:44 -0500

    chore(buy): remove retro disk (#374)

--- apps/buy.immich.app/src/routes/+page.svelte
+++ apps/buy.immich.app/src/routes/+page.svelte
@@ -6,11 +6,6 @@
 
   const serverFeatures = ['For the whole server', 'Lifetime purchase', 'Supporter status'];
   const userFeatures = ['For an individual', 'Lifetime purchase', 'Supporter status'];
-  const retroDiskFeatures = [
-    'Physical DVD with artwork',
-    'Bootable Immich Instance',
-    'Purchasable with Server or Individual Key',
-  ];
 </script>
 
 <FullPageLayout size="medium">
@@ -21,53 +16,13 @@
     </SupporterBadge>
     <section>
       <Stack gap={4}>
-        <div
-          class="gradiant-bg hover:bg-immich-primary/10 dark:hover:bg-immich-primary/20 w-full rounded-2xl border border-gray-300 p-6 dark:border-gray-800 dark:bg-gray-900"
-        >
-          <div class="">
-            <div class="flex w-full place-content-center place-items-center">
-              <img
-                src="https://imgproxy.fourthwall.com/DuRc1wOto_bXVrM-sqTsf17VxnxBXUFEQ5F_PT6sphQ/w:1920/sm:1/enc/CqJpqXXfIjg43qs5/dYp-7fXLhSxgi23i/vXvGKj_Wr12gyskx/XpH5YYXnecOLkEZD/Df2Y8QRXm5WNGa0M/tTxj3yhuyZNfwLfh/HIAL4muGjJJgH77a/m4-0PI8wYVzKDLfL/aKzTzjksdRXbrDIs/C3FH6IfFkB64QwBV/I4q06GTK8g4kyH_x/R_0WQk6JhX0eicl8/yzswcthmPd9IwLY-/daD-bA"
-                alt="Immich Retro Disk"
-                class="aspect-square w-full rounded-lg object-cover md:w-1/2"
-              />
-            </div>
-
-            <div
-              class="mt-6 flex flex-1 flex-col gap-4 sm:min-w-0 sm:flex-row sm:place-items-center sm:justify-between"
-            >
-              <div class="flex flex-col gap-2 sm:min-w-0 sm:flex-1">
-                <p class="text-primary retro-font text-4xl">Retro Disk</p>
-                <Stack gap={1}>
-                  {#each retroDiskFeatures as feature, index (index)}
-                    <div class="grid grid-cols-[36px_auto] items-center">
-                      <Icon icon={mdiCheckCircleOutline} size="24" class="text-success" />
-                      <Text>{feature}</Text>
-                    </div>
-                  {/each}
-                </Stack>
-              </div>
-
-              <div class="flex flex-col sm:flex-shrink-0 sm:items-end sm:justify-start">
-                <Heading size="giant" class="text-6xl sm:whitespace-nowrap">$25</Heading>
-                <Text class="sm:whitespace-nowrap">+ shipping</Text>
-              </div>
-            </div>
-          </div>
-
-          <div class="mt-6">
-            <Button href="https://immich.store/products/immich-retro" fullWidth size="large">Select</Button>
-          </div>
-        </div>
-        <hr />
-
         <div class="flex flex-wrap justify-between gap-4 lg:flex-nowrap">
           <!-- SERVER -->
           <div
             class="hover:bg-immich-primary/10 dark:hover:bg-immich-primary/20 flex w-full flex-col gap-4 rounded-2xl border border-gray-300 bg-gray-100 p-4 transition-all md:p-8 dark:border-gray-800 dark:bg-gray-900"
           >
             <div class="flex flex-col gap-4 sm:flex-row sm:items-start lg:flex-col lg:items-start">
-              <div class="w-full flex-shrink-0 sm:w-32 md:w-48 lg:w-full">
+              <div class="w-full shrink-0 sm:w-32 md:w-48 lg:w-full">
                 <div class="text-primary flex flex-col items-start gap-1">
                   <Icon icon={mdiServer} size="56" />
                   <Text fontWeight="bold" size="large">Server</Text>
@@ -84,7 +39,7 @@
                   {/each}
                 </Stack>
 
-                <div class="flex flex-col sm:flex-shrink-0 sm:items-end sm:justify-start lg:items-start">
+                <div class="flex flex-col sm:shrink-0 sm:items-end sm:justify-start lg:items-start">
                   <Heading size="giant" class="text-6xl sm:whitespace-nowrap">$100</Heading>
                   <Text class="sm:whitespace-nowrap">per server</Text>
                 </div>
@@ -99,7 +54,7 @@
             class="hover:bg-immich-primary/10 dark:hover:bg-immich-primary/20 flex w-full flex-col gap-4 rounded-2xl border border-gray-300 bg-gray-100 p-4 transition-all md:p-8 dark:border-gray-800 dark:bg-gray-900"
           >
             <div class="flex flex-col gap-4 sm:flex-row sm:items-start lg:flex-col lg:items-start">
-              <div class="w-full flex-shrink-0 sm:w-32 md:w-48 lg:w-full">
+              <div class="w-full shrink-0 sm:w-32 md:w-48 lg:w-full">
                 <div class="text-primary flex flex-col items-start gap-1">
                   <Icon icon={mdiAccount} size="56" />
                   <Text fontWeight="bold" size="large">Individual</Text>
@@ -116,7 +71,7 @@
                   {/each}
                 </Stack>
 
-                <div class="flex flex-col sm:flex-shrink-0 sm:items-end sm:justify-start lg:items-start">
+                <div class="flex flex-col sm:shrink-0 sm:items-end sm:justify-start lg:items-start">
                   <Heading size="giant" class="text-6xl sm:whitespace-nowrap">$25</Heading>
                   <Text class="sm:whitespace-nowrap">per user</Text>
                 </div>
@@ -145,43 +100,3 @@
     </Stack>
   </Stack>
 </FullPageLayout>
-
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Jersey+15&display=swap');
-  .retro-font {
-    font-family: 'Jersey 15', sans-serif;
-    font-weight: 400;
-    font-style: normal;
-  }
-  .gradiant-bg {
-    position: relative;
-    background: linear-gradient(
-      to right,
-      rgba(198, 221, 247, 0.25) 0%,
-      rgba(255, 240, 204, 0.25) 25%,
-      rgba(152, 191, 163, 0.25) 50%,
-      rgba(198, 221, 247, 0.25) 75%,
-      rgba(237, 190, 214, 0.25) 100%
-    );
-  }
-
-  :global(.dark) .gradiant-bg {
-    background: linear-gradient(
-      to right,
-      rgba(198, 221, 247, 0.11) 0%,
-      rgba(255, 240, 204, 0.11) 25%,
-      rgba(152, 191, 163, 0.11) 50%,
-      rgba(198, 221, 247, 0.11) 75%,
-      rgba(237, 190, 214, 0.11) 100%
-    );
-  }
-
-  :global(.dark) .gradiant-bg::before {
-    background: rgba(0, 0, 0, 0);
-  }
-
-  .gradiant-bg > * {
-    position: relative;
-    z-index: 1;
-  }
-</style>
