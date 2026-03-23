7f839747c289dfa18749b5583eaf3aa2ed0962e7
Zack <zack@futo.org>
2026-03-23 17:51:52 +0000

    fix: improve welcome page text contrast on dark background

--- apps/futo-backups-survey.immich.app/src/lib/components/WelcomeScreen.svelte
+++ apps/futo-backups-survey.immich.app/src/lib/components/WelcomeScreen.svelte
@@ -17,16 +17,16 @@
   <div class="flex max-w-[640px] flex-col gap-4 text-base">
     <h1 class="text-3xl font-bold">FUTO Backups</h1>
 
-    <p class="text-gray-500">
+    <p class="text-gray-300">
       Hey, thanks for taking the time to fill out the FUTO Backups survey. We estimate this will
       take a maximum of 5 minutes of your time. As a thanks for completing this survey, you can
       provide your email at the end and we will invite you to the closed beta of the product when it
       launches, which will be free for the duration of the beta period.
     </p>
 
-    <p class="text-gray-500">This survey is broken down in 6 sections;</p>
+    <p class="text-gray-300">This survey is broken down in 6 sections;</p>
 
-    <ol class="list-decimal space-y-1 pl-6 text-gray-500">
+    <ol class="list-decimal space-y-1 pl-6 text-gray-300">
       <li>Questions about your immich server, your library and your internet speed</li>
       <li>Your current backup solution</li>
       <li>Questions about FUTO Backups - and your interest in it</li>
