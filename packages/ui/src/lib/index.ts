da5b7f839461f3f5b410f92eef6e489bac4b68a5
Jason Rasmussen <jason@rasm.me>
2026-08-21 14:15:00 -0400

    fix: export persisted state

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -118,6 +118,7 @@ export * from '$lib/services/toast-manager.svelte.js';
 export * from '$lib/services/translation.svelte.js';
 export * from '$lib/state/locale-state.svelte.js';
 export { isModalOpen } from '$lib/state/modal-state.svelte.js';
+export * from '$lib/state/persisted.js';
 export * from '$lib/types.js';
 export * from '$lib/utilities/byte-units.js';
 export * from '$lib/utilities/common.js';
