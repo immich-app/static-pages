fa2e800f6e12b19dc059037f184894dd0ff47eb0
Jason Rasmussen <jason@rasm.me>
2026-08-25 10:27:49 -0400

    fix: build (#727)

--- packages/kysely-adapter-cloudflare/vite.config.ts
+++ packages/kysely-adapter-cloudflare/vite.config.ts
@@ -31,5 +31,5 @@ export default defineConfig({
       TZ: 'UTC',
     },
   },
-  plugins: [dts({ rollupTypes: true, tsconfigPath: './tsconfig.json' })],
+  plugins: [dts({ entryRoot: 'src', tsconfigPath: './tsconfig.json' })],
 });
